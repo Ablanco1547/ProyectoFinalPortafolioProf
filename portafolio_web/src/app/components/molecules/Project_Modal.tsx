6050ffc41196939e983f71442b83a17f7d51ba26
Ablanco1547 <ablancoc@ucenfotec.ac.cr>
2024-08-16 18:47:31 -0400

    Modal quick fix

--- portafolio_web/src/app/components/molecules/Project_Modal.tsx
+++ portafolio_web/src/app/components/molecules/Project_Modal.tsx
@@ -28,15 +28,32 @@ const Project_Modal: React.FC = () => {
 			</Button>
 
 			<Modal
-				className="modalProyect"
-				title="Nombre del proyecto"
+				className="modalProyect flex flex-col items-center gap-5"
+				title="Titulo del Proyecto"
 				centered
 				open={modal1Open}
 				onOk={() => setModal1Open(false)}
 				onCancel={() => setModal1Open(false)}
 				wrapClassName={!modal1Open ? "hidden-overlay" : ""}
 			>
-				<p>Aqui va toda la info del proyecto</p>
+				<iframe
+					width="480"
+					height="315"
+					src="https://www.youtube-nocookie.com/embed/7f0JdK5uphs?si=oHy5fTeIFPfO6rqf&amp;controls=0"
+					title="YouTube video player"
+					frameBorder="0"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+					referrerPolicy="strict-origin-when-cross-origin"
+				></iframe>
+				<p>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore
+					obcaecati porro necessitatibus nulla magnam quidem eos quo fuga, quia
+					omnis blanditiis asperiores consectetur expedita vitae et rem modi
+					ipsam ducimus.
+				</p>
+
+				<br></br>
+				<p className="text-portOffWhite">html, css, javascript, typescript</p>
 			</Modal>
 		</>
 	);
